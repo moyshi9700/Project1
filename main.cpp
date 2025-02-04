b81ff12d4a692228dd5c66343a3320ced7ac6e4b
moyshi9700 <75382965+moyshi9700@users.noreply.github.com>
2025-02-04 18:51:46 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+    //int question1();
+    //cout << question1() << endl;
+
+    //int question2();
+    //cout << question2() << endl;
+
+    int question3();
+    cout << question3() << endl;
+
+    //int question4();
+    //cout << question4() << endl;
+
+    //int question5();
+    //cout << question5() << endl;
+
+    //int question6();
+    //cout << question6() << endl;
+
+    return 0;
+}
